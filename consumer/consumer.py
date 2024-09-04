a55256097da6e8ce87c73f5d278d288f6921cbab
Towak4153 <bojan.grellinger@edu.teko.ch>
2024-09-04 13:33:57 +0200

    Update RabbitMQ credentials in consumer.py and docker-compose.yml

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -16,9 +16,12 @@ collection = db[mongodb_collection]
 # RabbitMQ-Einstellungen
 rabbitmq_host = os.getenv('RABBITMQ_HOST', 'localhost')
 rabbitmq_queue = os.getenv('RABBITMQ_QUEUE', 'MSFT')
+rabbitmq_user = os.getenv('RABBITMQ_USER', 'user')
+rabbitmq_pass = os.getenv('RABBITMQ_PASS', 'user')
 
 # Verbindung zu RabbitMQ herstellen
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
+credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_pass)
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host, credentials=credentials))
 channel = connection.channel()
 
 channel.queue_declare(queue=rabbitmq_queue)
